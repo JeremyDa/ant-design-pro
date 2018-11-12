ccda4d915f04f6cb461519ddcee6a7147ec20133
jeremy da <705653685@qq.com>
2018-11-12 08:14:23 +0800

    统一添加操作更新提示

--- src/models/table.js
+++ src/models/table.js
@@ -1,4 +1,5 @@
 import { query, remove, add, update } from '@/services/api';
+import { message } from 'antd';
 
 export default {
   namespace: 'table',
@@ -43,6 +44,9 @@ export default {
         payload: response,
       });
       if (callback) callback();
+      yield put({
+        type: 'new',
+      });
     },
     *remove({ payload, callback }, { select, call, put }) {
       const pagination = yield select(state => state.table.data.pagination);
@@ -52,6 +56,9 @@ export default {
         payload: response,
       });
       if (callback) callback();
+      yield put({
+        type: 'delete',
+      });
     },
     *update({ payload, callback }, { select, call, put }) {
       const pagination = yield select(state => state.table.data.pagination);
@@ -60,7 +67,11 @@ export default {
         type: 'save',
         payload: response,
       });
+
       if (callback) callback();
+      yield put({
+        type: 'edit',
+      });
     },
     *return({ payload, callback }, { call, put }) {
       const response = yield call(update, payload);
@@ -79,6 +90,16 @@ export default {
   },
 
   reducers: {
+    new() {
+      message.success('保存成功');
+    },
+    edit(state) {
+      message.success('更新成功');
+      return state;
+    },
+    delete() {
+      message.success('删除成功');
+    },
     save(state, action) {
       if (action.payload.list) {
         return {
