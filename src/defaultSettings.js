0ea31334585d70a66d19596daefcc1dda15ecf9f
jeremy da <705653685@qq.com>
2018-11-19 00:44:32 +0800

    update setting

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -1,9 +1,9 @@
 module.exports = {
-  navTheme: 'dark',
+  navTheme: 'light',
   primaryColor: '#1890FF',
   layout: 'topmenu',
   contentWidth: 'Fixed',
-  fixedHeader: false,
+  fixedHeader: true,
   autoHideHeader: false,
   fixSiderbar: false,
   collapse: true,
